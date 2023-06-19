ba164ae202a58ba706417bd318f2a32eeeb4278a
Angelo-Luiz <angelolfranz@gmail.com>
2023-06-19 16:11:30 -0300

    Validação de Login

---
+++ projeto/Controller/loginController.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('../Model/Postgres.php');
+
+$postgres = new Postgres('angelo', 'angelo', 'integrador');
+$postgres->criaConexao();
+$conn = $postgres->getConexao();
+if(!$conn){
+    header('Location: ../View/login.php?erro=1');
+}
+
+$email = $postgres->getConexao()->quote($_POST['email']);
+$senha = $postgres->getConexao()->quote($_POST['senha']);
+
+$query = "select * from usuarios where email = ". $email ." and senha = " . $senha ;
+
+$consulta = $conn->query($query);
+$consulta = $consulta->fetchAll();
+
+if(!$consulta){
+    header('Location: ../View/login.php?erro=2');
+} else{
+    session_start();
+    $_SESSION['id_usuario'] = $consulta[0]['id'];;
+    $postgres->desconectar();
+    header('Location: ../View/Home.php?login=success');
+}
+
+
+
+
+
+
+
